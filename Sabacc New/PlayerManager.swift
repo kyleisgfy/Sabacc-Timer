1d5719bd00c285239d82b2f891c04d7a900ad312
Kyle Schneider <kyleschneider@Kyles-MacBook-Pro.local>
2016-09-13 20:47:47 -0700

    bunch of changes including some animations

---
+++ Sabacc New/PlayerManager.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerManager.swift
+//  Sabacc New
+//
+//  Created by Kyle Schneider on 9/9/16.
+//  Copyright © 2016 Kyle Schneider›‹. All rights reserved.
+//
+
+import Cocoa
+
+class PlayerManager: NSObject {
+
+}
---
+++ ThirdViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.swift
+//  Sabacc New
+//
+//  Created by Kyle Schneider on 9/11/16.
+//  Copyright © 2016 Kyle Schneider›‹. All rights reserved.
+//
+
+import Cocoa
+
+class ThirdViewController: UIViewController {
+
+}
