06bfa74083f62ed5143ac4eac31b4e4c16b7b16c
Kyle Schneider <kyletschneider@me.com>
2016-09-18 12:52:11 -0700

    -Need to adjust the width of the label so that it is the length of the view. I did see that this is done with a setting  arrange: stream horizontally (seen in secondViewController test  button), however i need  it don't codally because that is where I'm creating the label.
    - want to add swipe from page to page
    - need to save animation state in secondViewController and resume when view is on screen.
    - still need to get timer working.
    - would like to make the label not look like they are visiting from 1997.
    - would like to have the listView in the firstViewController default to have non committed players from the previous game so that you can click on the name and commit it to the structure array. maybe a button that takes the name and adds it to the array if pressed and if a different name is added, button is removed from view?

--- ThirdViewController.swift
+++ ThirdViewController.swift
@@ -12,16 +12,12 @@ class ThirdViewController: UIViewController {
     
 //      ///Buttons
     @IBOutlet weak var resetButton: UILabel!
+    @IBOutlet weak var stackViewMain: UIStackView!
     
     
 //      ///Labels
-    @IBOutlet weak var labelOne: UILabel!
-    @IBOutlet weak var labelTwo: UILabel!
-    @IBOutlet weak var labelThree: UILabel!
-    @IBOutlet weak var labelFour: UILabel!
-    @IBOutlet weak var labelFive: UILabel!
-    @IBOutlet weak var labelSix: UILabel!
-    var labelArray : [UILabel] = []
+    
+    var labelArray = Array (repeating: UILabel (), count: playerMgr.players.count)
 
 //View Did Load
     
@@ -44,17 +40,23 @@ class ThirdViewController: UIViewController {
         print ("view 3 Did Layout Subviews")
         super.viewDidLayoutSubviews()
         
+//      Creates a label for each player and puts it in the array "labelArray".
+//      Puts the array in the stack view named "stackViewMain" with append.
+//      Centers the text of the label.
+//      Allows the text to be adjusted if it exceeds the length of alotted space for thw label.
 //      Itterates through the list of labels and sets them to alpha 0.
 //      This makes them transparent to allow fade in animation.
-//      Additionaly, changes the text of each label to the names in the player list.
-        
-        labelArray = [labelOne, labelTwo, labelThree, labelFour, labelFive, labelSix]
+//      Changes the text of each label to the names in the player list.
         
         for index in 0...(playerMgr.players.count - 1) {
-            print("Set label \(index) alpha to 0.")
-            self.labelArray[index].alpha = 0
-            print("Assign label \(index) to name \(playerMgr.players[index].name)")
-            self.labelArray[index].text = "\(playerMgr.players[index].name)"
+            let label = UILabel()
+            labelArray.append (label)
+            labelArray[index].textAlignment = NSTextAlignment.center
+            labelArray[index].adjustsFontSizeToFitWidth = true
+   //         labelArray[index].frame.width = (50, 150, 200, 21)
+            labelArray[index].alpha = 0
+            labelArray[index].text = "\(playerMgr.players[index].name)"
+            self.stackViewMain.addArrangedSubview(labelArray[index])
         }
 
 //      Changes the background color of each label to:
@@ -74,33 +76,28 @@ class ThirdViewController: UIViewController {
         }
         
         
+    
     }
     
-    override func viewDidAppear(_ animated: Bool){
+    override func viewDidAppear(_ animated: Bool) {
         print("view3DidLayoutSubviews")
         super.viewDidAppear(animated)
 //      Animation to fade in labels
+//      Fades labels in to 50% alpha if they are green and player does not shift.
+//      Fades labels in to 70% alpha if they are yellow and player shifts one card.
+//      Fades labels in to 100% alpha if the are red and player shifts two cards.
         
-        UIView.animateWithDuration( 2, animations:  {
+        UIView.animate(withDuration : 2, delay: 0, options: [], animations: {
         for index in 0...(playerMgr.players.count - 1) {
             if playerMgr.players[index].shift == false {
-                self.labelArray[index].alpha = 50
+                self.labelArray[index].alpha = 0.3
             } else if playerMgr.players[index].modifier == true {
-                self.labelArray[index].alpha = 100
+                self.labelArray[index].alpha = 1
             } else {
-                self.labelArray[index].alpha = 70
+                self.labelArray[index].alpha = 0.5
                 }
             }
-        }
+        } , completion: nil )
 
+    }
 }
-}
-
-
-
-
-    
-
-
-
-
