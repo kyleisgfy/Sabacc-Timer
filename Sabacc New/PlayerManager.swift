acdd7b6bf8aaccf8edc1f4f67f743e9970f39d18
Christopher Schneider <cd_schneider@apple.com>
2016-09-24 01:18:33 -0700

    Started implimenting plist persistence in PlayerManager. Still not done... Need to create a function to utilize the mutable dictionary but file creation is complete.

--- Sabacc New/PlayerManager.swift
+++ Sabacc New/PlayerManager.swift
@@ -10,7 +10,41 @@ import UIKit
 
 var playerMgr: PlayerManager = PlayerManager ()
 
+var paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString
+var path = paths.appendingPathComponent("data.plist")
+let save = NSDictionary(contentsOfFile: path)
 
+// Get the documents Directory
+func documentsDirectory() -> String {
+    let documentsFolderPath = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
+    return documentsFolderPath
+}
+
+// Get path for a file in the directory
+func fileInDocumentsDirectory(filename: String) -> String {
+    
+    let writePath = (documentsDirectory() as NSString).appendingPathComponent("Players")
+    
+    if (!FileManager.default.fileExists(atPath: writePath)) {
+        do {
+            try FileManager.default.createDirectory(atPath: writePath, withIntermediateDirectories: false, attributes: nil) }
+        catch let error as NSError {
+            print(error.localizedDescription);
+        }
+    }
+    return (writePath as NSString).appendingPathComponent(filename)
+}
+
+//func savePlayer (player: String, path: String ){
+//    
+//    let playerData = String.self
+//    
+////   let result = playerData.writeToFile(path, atomically: true)
+//    
+////    print("\(result)")
+////    print("\(path)")
+//    
+//}
 
 struct structureOfPlayer{
     var name = "String"
@@ -32,6 +66,7 @@ class PlayerManager: NSObject {
             shift: false,
             modifier: false,
             playersCurrentDiceRoll: 0))
+//        savePlayer(player: name, path: path)
     
     
     }
